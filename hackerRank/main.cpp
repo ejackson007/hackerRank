bdc0dfd3d2d52bb5b6022eb93040f02cd0b600aa
Evan Jackson <evanojackson@gmail.com>
2020-08-16 19:51:26 -0500

    First commit

---
+++ hackerRank/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  hackerRank
+//
+//  Created by Evan Jackson on 8/16/20.
+//  Copyright © 2020 Evan Jackson. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
